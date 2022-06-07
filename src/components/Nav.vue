8381d101feecf67c5d465cbc5449fb30b908af04
Saiful Islam Meraz <42317343+meraz007@users.noreply.github.com>
2022-06-07 19:19:10 +0600

    responsive--homeView

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,36 +1,121 @@
 <template>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
-  <div class="container flex mx-auto">
-    <a class="flex items-center">
-        <router-link to="/" class="self-center text-xl font-semibold whitespace-nowrap text-gray-500">Text-Gen</router-link>
-    </a>
-    <div class="flex justify-center w-full md:block " id="mobile-menu">
-      <ul class="flex flex-col justify-center mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-        <li>
-          <a class="block py-2 pr-4 pl-3 text-gray-700 cursor-pointer">Services</a>
-        </li>
-        <li>
-          <a class="block py-2 pr-4 pl-3 text-gray-700 cursor-pointer">Pricing</a>
-        </li>
-        <li>
-          <a class="block py-2 pr-4 pl-3 text-gray-700 cursor-pointer">Contact</a>
-        </li>
-      </ul>
+  <div>
+    <div class="bg-white">
+      <nav
+        class="
+          container
+          px-6
+          py-8
+          mx-auto
+          md:flex md:justify-between md:items-center
+        "
+      >
+        <div class="flex items-center justify-between">
+          <router-link
+            to="/"
+            class="
+              text-xl
+              font-bold
+              text-gray-800
+              md:text-2xl
+              hover:text-blue-400
+            "
+            >Logo
+          </router-link>
+          <!-- Mobile menu button -->
+          <div @click="navigate" class="flex md:hidden">
+            <button
+              type="button"
+              class="
+                text-gray-800
+                hover:text-gray-400
+                focus:outline-none focus:text-gray-400"
+            >
+            <NavBar/>
+            </button>
+          </div>
+        </div>
+
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <div class="flex w-full md:block " id="mobile-menu">
+        <ul
+          :class="showMenu ? 'flex' : 'hidden'"
+          class="
+            flex-col
+            justify-center
+            mt-8
+            space-y-4
+            md:flex
+            md:space-y-0
+            md:flex-row
+            md:items-center
+            md:space-x-10
+            md:mt-0
+          "
+        >
+          <li class="block py-2 pr-4 pl-3 text-gray-700 cursor-pointer">
+            Services
+          </li>
+          <li class="block py-2 pr-4 pl-3 text-gray-700 cursor-pointer">
+            Prices
+          </li>
+          <li class="block py-2 pr-4 pl-3 text-gray-700 cursor-pointer">
+            Contact
+          </li>
+        </ul>
+        </div>
+        <div :class="showMenu ? 'flex' : 'hidden'"
+          class="
+            flex-col
+            justify-center
+            mt-8
+            space-y-4
+            md:flex
+            md:space-y-0
+            md:flex-row
+            md:items-center
+            md:space-x-10
+            md:mt-0
+          ">
+          <router-link to="/Signin" 
+            class="block
+            py-2 pr-4
+            pl-3 w-24
+          text-gray-700
+            cursor-pointer">
+            Sign In
+          </router-link>
+          <router-link to="/signup" 
+            class="block py-2 
+            pr-4 pl-3 w-24 
+          bg-black 
+          text-white
+            rounded-md 
+            cursor-pointer">
+            Sign Up
+          </router-link>
+          </div>
+      </nav>
     </div>
-    <div class="flex flex-row">
-      <router-link to="/Signin" class="block py-2 pr-4 pl-3 w-24 text-gray-700 cursor-pointer">Sign In</router-link>
-      <router-link to="/signup" class="block py-2 pr-4 pl-3 w-24 bg-black text-white rounded-md cursor-pointer">
-        Sign Up</router-link>
   </div>
-  </div>
-</nav>
 </template>
 
 <script>
-import ButtonSimple from '@/components/ButtonSimple.vue'
+import NavBar from '@/components/Svg/NavBar.vue'
 export default {
-    components:{
-        ButtonSimple
+  components:{
+    NavBar
+  },
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods:{
+    navigate(){
+      this.showMenu =!this.showMenu;
     }
-}
+  }
+};
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <div class="flex justify-center mt-36">
       <div class="w-7/12">
-        <h1 class="text-5xl font-bold">A better, 10x faster way to write</h1>
-        <h2 class="text-center">Rytr is an AI writing assistant that helps you create high-quality content, in just a few seconds, at a fraction of the cost!</h2>
+        <h1 class="text-3xl text-center md:text-5xl font-bold">A better, 10x faster way to write</h1>
+        <h2 class="text-center py-4">Rytr is an AI writing assistant that helps you create high-quality content, in just a few seconds, at a fraction of the cost!</h2>
         <div class="flex justify-center">
         <router-link to="/about" class="bg-orange-600 px-6 py-2 font-semibold text-white rounded-md hover:opacity-95 focus:outline-none ">text-gen</router-link>
         </div>
@@ -12,10 +12,10 @@
   <div class="flex justify-center mt-20">
     <h1 class="text-4xl font-bold text-gray-900">Client Feedback</h1>
   </div>
-    <div class="flex gap-8 mt-12 justify-center">
+  <div class="grid justify-center content-center gap-4 md:grid-flow-col mt-12">
       <Card />
       <Card />
-  </div>
+      </div>
   <Footer class="mt-4"></Footer>
   </div>
 </template>
@@ -28,7 +28,10 @@ export default {
   name: 'HomeView',
   components: {
     Card,
-    Footer
+    Footer,
   }
 }
 </script>
+
+<style scoped>
+</style>
